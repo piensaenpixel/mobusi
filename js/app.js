6e51f777081fb23d0c1c361ae4f8a15ff6b7456c
piensaenpixel <piensaenpixel@gmail.com>
2016-06-09 23:44:55 +0200

    add changes

--- js/app.js
+++ js/app.js
@@ -79,12 +79,25 @@ $(document).ready(function () {
     });
 
 
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+    
+        $(document).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });  
+
 
 });
 
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
-
     $('.navigation-item a').each(function () {
         var currLink = $('navigation-itemLink');
         var refElement = $(currLink.attr("href"));
@@ -97,3 +110,7 @@ function onScroll(event){
         }
     });
 }
+
+
+
+
