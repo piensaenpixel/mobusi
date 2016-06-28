7df44d94b9681a91b9bffa80fb173e9235e61747
piensaenpixel <piensaenpixel@gmail.com>
2016-06-28 21:15:25 +0200

    fixes

--- js/app.js
+++ js/app.js
@@ -117,7 +117,7 @@ $(document).ready(function () {
         $target = $(target);
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
+        }, 2000, 'easeOutCubic', function () {
             window.location.hash = target;
             $(document).on("scroll", onScroll);
         });
